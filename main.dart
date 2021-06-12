b63f5b50b6d1cbcbfdcc470cb77284ae0f240a45
peter oyebamiji <peter2olumide@yahoo.com>
2021-06-12 22:09:14 +0000

    keep learning

--- main.dart
+++ main.dart
@@ -1,3 +1,43 @@
+import "dart:math";
+import "dart:io";
+
+//declaring function
+void drawTriangle(){
+  print("|-|");
+  print("|-|");
+}
+
 void main() {
-	print('Hello World!');
+  /*print("Enter a color: ");
+  String color = stdin.readLineSync();
+
+  print("Enter a plural noun: ");
+  String pluralNoun = stdin.readLineSync();
+  
+  print("Enter a celebrity: ");
+  String celebrity = stdin.readLineSync();
+
+  print("My body color is: ${color}");
+  print("I love ${pluralNoun}");
+  print("${celebrity} is my favourite celebrity"); 
+
+//function
+drawTriangle();
+
+ //basic calculator
+  print("Enter first Number:");
+  //to covert string to number for calculation add "double" to the variable declaration
+  double num1 = double.parse(stdin.readLineSync());
+  print("Enter second Number:");
+  double num2 = double.parse(stdin.readLineSync());
+  
+  print(
+    num1 + num2
+  );  */
+  
+  //array or list
+List<int> favNums = [4, 5, 9, 16, 20, 70];
+
+
+print(favNums.length); 
 }
